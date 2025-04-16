9dfcd939826cd33ef75070263898808a763c5c01
Dhasneem Beevi <dhasmohamed1020@gmail.com>
2025-04-16 15:36:29 +0530

    updated gitignore - Price Tracker Backend

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# Optional: set display variable for headless Chrome
 export DISPLAY=:99
 
-# Start the FastAPI app
+# Start FastAPI app
 uvicorn main:app --host 0.0.0.0 --port ${PORT:-5000}
-
